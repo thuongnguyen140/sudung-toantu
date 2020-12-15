03e6312a656b50bd7570de3da584e73d292ad056
THUONG <hoaithuong.nht@gmail.com>
2020-12-16 03:19:40 +0700

    sudung-toantu

---
+++ toantu.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("nhập chiều rộng: ");
+let inputHeight = prompt("nhập chiều dài: ");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích bằng: " + area);
